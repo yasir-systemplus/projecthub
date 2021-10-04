082847d77ee347281873414e19e3a8fe7b11c900
Muhammad Yasir <shahzadyasir77@gmail.com>
2021-10-04 14:11:53 +0500

    Done with AWS Amplify Configuration

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
 
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
-
+import Amplify from 'aws-amplify';
+import config from './src/aws-exports';
 import App from './src/App';
+
+Amplify.configure(config);
 
 AppRegistry.registerComponent(appName, () => App);
