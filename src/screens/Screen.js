ab748dc11ebfe7102e01b981c2b5a60bbdba1f1e
Muhammad Yasir <shahzadyasir77@gmail.com>
2021-10-04 13:00:24 +0500

    AWS Profile Creation and Hiding Creds with .gitIgnore

---
+++ src/screens/Screen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+export default function Screen({children}) {
+  return <View style={styles.container}>{children}</View>;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    //paddingTop: StatusBar.currentHeight,
+  },
+});
