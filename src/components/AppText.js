d65997666660a981fd62542dd2a886d3fa5f4594
Muhammad Yasir <shahzadyasir77@gmail.com>
2021-10-08 13:04:57 +0500

    Implemented other props on text

--- src/components/AppText.js
+++ src/components/AppText.js
@@ -2,6 +2,10 @@ import React from 'react';
 import {Text} from 'react-native';
 import defaultStyles from '../config/typography';
 
-export default function AppText({children}) {
-  return <Text style={defaultStyles.bodyLarge}>{children}</Text>;
+export default function AppText({children, ...otherProps}) {
+  return (
+    <Text style={defaultStyles.bodyLarge} {...otherProps}>
+      {children}
+    </Text>
+  );
 }
