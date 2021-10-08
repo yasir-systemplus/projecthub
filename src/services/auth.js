a0321489977aae564903c7fe070a022863e4374c
Muhammad Yasir <shahzadyasir77@gmail.com>
2021-10-08 13:06:08 +0500

    Added new password challenge method

--- src/services/auth.js
+++ src/services/auth.js
@@ -9,3 +9,16 @@ import {Auth} from 'aws-amplify';
 export const login = (username, password) => {
   return Auth.signIn({username, password});
 };
+
+export const completeNewPassword = (user, newPassword) => {
+  // const {requiredAttributes} = user.challengeParam; // the array of required attributes, e.g ['email', 'phone_number']
+  return Auth.completeNewPassword(
+    user, // the Cognito User Object
+    newPassword, // the new password
+    // OPTIONAL, the required attributes
+    // {
+    //   email: 'xxxx@example.com',
+    //   phone_number: '1234567890',
+    // },
+  );
+};
