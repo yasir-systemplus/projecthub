87127276c83c4769ff7865c051731f33d145ce65
Muhammad Yasir <shahzadyasir77@gmail.com>
2021-10-08 13:05:37 +0500

    Increase width and height using Dimensions API

--- src/screens/Screen.js
+++ src/screens/Screen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, Dimensions} from 'react-native';
 
 export default function Screen({children}) {
   return <View style={styles.container}>{children}</View>;
@@ -10,5 +10,7 @@ const styles = StyleSheet.create({
     //paddingTop: StatusBar.currentHeight,
     paddingHorizontal: 16,
     flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
