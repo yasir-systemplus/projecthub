d498cdae3691138dd3280eccae2daef9ef5a060f
Muhammad Yasir <shahzadyasir77@gmail.com>
2021-10-12 14:58:39 +0500

    Storage Service

---
+++ src/services/storage.js
@@ -0,0 +1,19 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const setData = async (key, value) => {
+  try {
+    const jsonValue = JSON.stringify(value);
+    await AsyncStorage.setItem(key, jsonValue);
+  } catch (e) {
+    console.log('Error while saving on local storage');
+  }
+};
+
+export const getData = async key => {
+  try {
+    const jsonValue = await AsyncStorage.getItem(key);
+    return jsonValue != null ? JSON.parse(jsonValue) : null;
+  } catch (e) {
+    console.log('Error while receiving from local storage');
+  }
+};
