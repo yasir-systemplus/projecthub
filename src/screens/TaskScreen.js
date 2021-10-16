a7a3e69ba10979e40a22d3860506e97ee9b86ec4
Muhammad Yasir <shahzadyasir77@gmail.com>
2021-10-16 18:59:38 +0500

    Rendering comments with section lisrt

--- src/screens/TaskScreen.js
+++ src/screens/TaskScreen.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import {View, StyleSheet, ScrollView, SectionList} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  SectionList,
+  Dimensions,
+} from 'react-native';
 import CurvedPanHeader from '../components/CurvedPanHeader';
 import CurvedBodyPan from '../components/CurvedBodyPan';
 import AppText from '../components/AppText';
@@ -14,6 +20,7 @@ import ImageInput from '../components/ImageInput';
 import CommentsPan from '../components/CommentsPan';
 import * as faker from 'faker';
 import Comment from '../components/Comment';
+import colors from '../config/colors';
 const profiles = [
   {id: 1, image: 'https://picsum.photos/200'},
   {id: 2, image: 'https://picsum.photos/200'},
@@ -35,6 +42,15 @@ const comments = [
       'We would need to revamp colors. In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.',
     commentor: 'Bilal Shahzad',
     profile: faker.internet.avatar(),
+    data: [
+      {
+        id: 3,
+        message:
+          'We need permission. In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. From our boss to implement changes.',
+        commentor: 'Ali Azmat',
+        profile: faker.internet.avatar(),
+      },
+    ],
   },
   {
     id: 2,
@@ -42,6 +58,15 @@ const comments = [
       'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.Yes, I am making a new theme. Lor ',
     commentor: 'Asif Khan',
     profile: faker.internet.avatar(),
+    data: [
+      {
+        id: 1,
+        message:
+          'We would need to revamp colors. In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.',
+        commentor: 'Bilal Shahzad',
+        profile: faker.internet.avatar(),
+      },
+    ],
   },
   {
     id: 3,
@@ -49,77 +74,81 @@ const comments = [
       'We need permission. In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. From our boss to implement changes.',
     commentor: 'Ali Azmat',
     profile: faker.internet.avatar(),
+    data: [
+      {
+        id: 2,
+        message:
+          'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.Yes, I am making a new theme. Lor ',
+        commentor: 'Asif Khan',
+        profile: faker.internet.avatar(),
+      },
+    ],
   },
 ];
 export default function TaskScreen({navigation}) {
   return (
-    <>
-      <SectionList
-        ListHeaderComponent={
-          <>
-            <CurvedPanHeader
-              onBack={() => navigation.navigate(routes.PROJECT)}
+    <SectionList
+      ListHeaderComponent={
+        <>
+          <CurvedPanHeader onBack={() => navigation.navigate(routes.PROJECT)} />
+          <CurvedBodyPan>
+            <AppText style={[typography.heading4, styles.title]}>
+              Alo Pakory Banne hen, Workshop aur Hackathon
+            </AppText>
+            <ProfileImageWithRightDescription
+              image="https://picsum.photos/200"
+              role="Team Lead"
+              name="Muhammad Yasir"
             />
-            <CurvedBodyPan>
-              <AppText style={[typography.heading4, styles.title]}>
-                Alo Pakory Banne hen, Workshop aur Hackathon
-              </AppText>
-              <ProfileImageWithRightDescription
-                image="https://picsum.photos/200"
-                role="Team Lead"
-                name="Muhammad Yasir"
+            <AppText
+              style={[typography.bodyLarge, {marginTop: 5, marginBottom: 5}]}>
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+              quae ab illowe3 inventore veritatis et quasi architecto beatae
+              vitae dicta sunt explicabo.
+            </AppText>
+            <View style={styles.statusBar}>
+              <IconWithDescription
+                name="md-flag"
+                color="red"
+                description="03/12/2021"
+                component={Ionicons}
+                border={{}}
               />
-              <AppText
-                style={[typography.bodyLarge, {marginTop: 5, marginBottom: 5}]}>
-                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-                quae ab illowe3 inventore veritatis et quasi architecto beatae
-                vitae dicta sunt explicabo.
-              </AppText>
-              <View style={styles.statusBar}>
-                <IconWithDescription
-                  name="md-flag"
-                  color="red"
-                  description="03/12/2021"
-                  component={Ionicons}
-                  border={{}}
-                />
-                <AppBadge
-                  information="High"
-                  color="#fd9b35"
-                  textColor="white"
-                />
-                <AppBadge
-                  information="In-Progress"
-                  color="#8869ff"
-                  textColor="white"
-                />
-              </View>
-              <AppText style={typography.heading4}>Assigned To</AppText>
+              <AppBadge information="High" color="#fd9b35" textColor="white" />
+              <AppBadge
+                information="In-Progress"
+                color="#8869ff"
+                textColor="white"
+              />
+            </View>
+            <AppText style={typography.heading4}>Assigned To</AppText>
 
-              <ProfileBar profiles={profiles} />
-              <AppText style={typography.heading4}>Gallery</AppText>
+            <ProfileBar profiles={profiles} />
+            <AppText style={typography.heading4}>Gallery</AppText>
 
-              <View style={styles.gallery}>
-                <ScrollView horizontal>
-                  {gallery.map(g => (
-                    <ImageInput key={g.id} source={g.image} />
-                  ))}
-                </ScrollView>
-              </View>
-            </CurvedBodyPan>
-          </>
-        }
-        sections={comments}
-        renderSectionHeader={({section}) => (
-          <Comment comment={item} />
-        )}
-        keyExtractor={(item, index) => item.id}
-        renderItem={({item}) =>}
-      />
-
-      <CommentsPan />
-    </>
+            <View style={styles.gallery}>
+              <ScrollView horizontal>
+                {gallery.map(g => (
+                  <ImageInput key={g.id} source={g.image} />
+                ))}
+              </ScrollView>
+            </View>
+          </CurvedBodyPan>
+        </>
+      }
+      sections={comments}
+      renderItem={({item}) => <Comment comment={item} style={styles.reply} />}
+      renderSectionHeader={({section}) => (
+        <>
+          <Comment comment={section} />
+          <AppText style={{marginLeft: 87, color: colors.heading}}>
+            Reply
+          </AppText>
+        </>
+      )}
+      keyExtractor={(item, index) => index}
+    />
   );
 }
 
@@ -136,5 +165,11 @@ const styles = StyleSheet.create({
   gallery: {
     flexDirection: 'row',
     marginVertical: 15,
+  },
+  reply: {
+    borderLeftWidth: 1,
+    borderColor: 'black',
+    paddingLeft: 60,
+    marginVertical: 12,
   },
 });
