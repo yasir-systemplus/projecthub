0ab63b38e16c4784a439f95f7fd98441e88a2377
Muhammad Yasir <shahzadyasir77@gmail.com>
2021-10-16 19:00:21 +0500

    Adding style prop to AppText

--- src/components/AppText.js
+++ src/components/AppText.js
@@ -2,9 +2,9 @@ import React from 'react';
 import {Text} from 'react-native';
 import defaultStyles from '../config/typography';
 
-export default function AppText({children, ...otherProps}) {
+export default function AppText({children, style, ...otherProps}) {
   return (
-    <Text style={defaultStyles.bodyLarge} {...otherProps}>
+    <Text style={[defaultStyles.bodyLarge, style]} {...otherProps}>
       {children}
     </Text>
   );
